192dcad4809d73328b79e46f9158aae6ce46dc8d
izackk26 <newxstorecr@gmail.com>
2024-03-29 10:17:48 -0600

    feat: GithubPages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'izackk26.github.io',
+  base: '/PortfolioV2',
   integrations: [tailwind()]
 });
