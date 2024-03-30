968ce26658c9cc45eb99275cb68459f9a71b65f3
izackk26 <newxstorecr@gmail.com>
2024-03-29 23:32:41 -0600

    feat: minor changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+	content: [
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+		"./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    	"./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}"
+	],
 	theme: {
-		extend: {},
+	  extend: {},
 	},
 	plugins: [],
-}
+  });
