42f868d976cc525488d308e32a9625be103ee133
izackk26 <newxstorecr@gmail.com>
2024-05-11 15:48:06 +0000

    URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://izackk26.github.io',
-  base: '/',
+  base: 'webPortfolio',
   integrations: [tailwind(), react()]
 });
